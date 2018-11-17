c5f0c5ff23550f24f099392e59b8bb59ae79800d
st599 <st599@zepler.net>
2018-11-17 18:03:44 +0000

    Initial Commit - reads data from BME280 and writes to SD Card

--- Cycling-DataLogger/Cycling-DataLogger.ino
+++ Cycling-DataLogger/Cycling-DataLogger.ino
@@ -17,58 +17,174 @@
  *    Version 0.1 - Basic setup to record Text to an SD Card
  */
 
+ 
 
-// Require Libraries
 
+
+//////////////////////////////////////
+// Required Libraries
+//////////////////////////////////////
+#include <MinimumSerial.h>
+#include <BlockDriver.h>
+#include <sdios.h>
+#include <SysCall.h>
+#include <SdFatConfig.h>
+#include <SdFat.h>
+#include <FreeStack.h>
 #include <SPI.h>
-#include <SD.h>
+#include <Wire.h>
+#include <RTClib.h>              
+#include <Adafruit_BME280.h>
+
+ 
+
+
+ 
+//////////////////////////////////////
+// Class Instantiations
+//////////////////////////////////////
+SdFat SD;                                                                                                                                            
+RTC_DS1307 RTC;
+Adafruit_BME280 bme;                                                         // The BME280 will be an I2C device
+
+ 
+
+
+ 
+//////////////////////////////////////
+// Definitions and Global Variables
+//////////////////////////////////////
+float hgInches = 29.921;                                                     // Enter the sealevel barometric pressure here (xx.xx inches Hg)
+char TmeStrng[] = "0000/00/00,00:00:00";                                     // 19 ascii characters
+#define SEALEVELPRESSURE_HPA (hgInches/0.02952998751)                        // hPa=(inches Hg)/0.02952998751
+#define MOSIpin 11
+#define MISOpin 12
+#define DS1307_I2C_ADDRESS 0x68
+#define delay_ms 30000
+ 
 
 
 
-long record = 0;
+ 
+//////////////////////////////////////
+// SETUP FUNCTION
+//////////////////////////////////////
+void setup() { 
 
+  Serial.begin(9600);                                                         // Open serial communications and wait for port to open:
+  Wire.begin();                                                               // start the i2c interface
+  RTC.begin();                                                                // start the RTC
 
+  RTC.adjust(DateTime((__DATE__), (__TIME__)));                               //sets the RTC to the computer time when the sketch is loaded
+                                                                              // NB COMMENT OUT AFTER FIRST USE
 
-// Setup the arduino board
-void setup() {
+  Serial.println("Cycling Datalogger - v0.1 - 16/11/2018");
+
   
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+  // Test Card is Available
+  Serial.print("...Find SD card: ");
+
+  if (!SD.begin(10)) {
+    Serial.println("......Card failed");
   }
-  printHeaderSerial();
-  
-}
+  else  {
+    Serial.println("......SD card OK");  
+  }
 
-// Repeating function
+  // Print a file to the SD Card
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  if (dataFile) {                                                             // if the file is available, write to it:
+    dataFile.println("Nano Logger");
+    dataFile.close();
+  }
+  else {
+    Serial.println("......file error");                                       // if the file isn't open, pop up an error:
+  }
+
+
+  // Load BME280 Sensor
+  bool status;     
+  status = bme.begin(0x76);                                                   // 0x76 is the I2C address of the clone sensors I have
+  if (status) {
+    dataFile.println("...BME280 Sensor Init");
+    Serial.println("...BME280 Sensor Init");
+  }
+  else  {
+    Serial.println("......No BME280");
+    while (1);
+  }
+
+  Serial.println();
+
+}                                                                             // END OF SETUP FUNCTION
+
+ 
+
+
+ 
+//////////////////////////////////////
+// LOOP FUNCTION
+//////////////////////////////////////
 void loop() {
-  // Variables
-
-  float temp = 35.0;
-  float pressure = 1012.0;
   
-  Serial.print("Data Record: ");
-  Serial.println(record);
+  // Read Data from BME Sensor - Temp Humidity Pressure Altitude
+  float BMEt = (bme.readTemperature());
+  float BMEh = (bme.readHumidity());
+  float BMEp = (bme.readPressure() / 100.0F);
+  float BMEa = (bme.readAltitude(SEALEVELPRESSURE_HPA));
+ 
+  // Read time from realtime clock
+  DateTime now = RTC.now();                                              //this reads the time from the RTC
 
-  // Atmospheric Sensor
-  Serial.println("...Atmospheric Sensor");
-  Serial.print("......Temperature: ");
-  Serial.println(temp, 1);
-  Serial.print("......Pressure: ");
-  Serial.println(pressure, 0);
+  // Construct time string - format YYYY/MM/DD,HH:MM:SS
+  sprintf(TmeStrng, "%04d/%02d/%02d,%02d:%02d:%02d", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
 
+  // Print data to terminal
+  Serial.print("RTC utc Time: ");
+  Serial.print(now.unixtime());
+  Serial.println();
+  Serial.print("RTC time: ");
+  Serial.println(TmeStrng);
+  Serial.print("BME280 temp: ");
+  Serial.print(BMEt);
+  Serial.println(" C");
+  Serial.print("BME280 Humidity: ");
+  Serial.print(BMEh);
+  Serial.println(" %");
+  Serial.print("Pressure: ");
+  Serial.print(BMEp);
+  Serial.println(" hPa");
+  Serial.print("Elevation: ");
+  Serial.print(BMEa);
+  Serial.println(" m");
+  Serial.println();
+         
+
+  //Construct a data string to write to SD card
+  String dataString = "";                                           //this erases the previous string
+  dataString += TmeStrng;
+  dataString += ",";     
+  dataString += String(BMEt);
+  dataString += ",";  
+  dataString += String(BMEh);
+  dataString += ",";   
+  dataString += String(BMEp);
+  dataString += ",";   
+  dataString += String(BMEa);
+ 
+  // Write the data to the SD card
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);               // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+  }
+  else {
+    Serial.println("file error");                                   // if the file isn't open, pop up an error:
+  }
+
+  // Pause
+  delay(delay_ms);                       
   
-  // Increment Record Number and Wait
-  record++;
-  delay(5000);
-}
+}                                                                   // END of the MAIN LOOP
 
-
-
-// Print Header Data to Serial
-void printHeaderSerial()  {
-  Serial.println("Cycling Data Logger");
-  Serial.println("v1.0 27/10/18");
-  Serial.println("");
-}
+ 
